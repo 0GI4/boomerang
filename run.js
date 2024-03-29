c1e6289046081a6778fdcf5aa66fab892b238b91
0GI4 <pop9991@outlook.com>
2024-03-29 11:36:59 +0300

    shit

--- run.js
+++ run.js
@@ -1,12 +1,24 @@
 // Основной файл.
 // Запускает игру.
+const keypress = require('keypress');
 const Game = require('./src/Game');
+const View = require('./src/View');
+const Boomerang = require('./src/game-models/Boomerang');
+const Hero = require('./src/game-models/Hero');
+const Enemy = require('./src/game-models/Enemy');
+const runInteractiveConsole = require('./src/keyboard');
+
+const hero1 = new Hero(0);
 
 // Инициализация игры с настройками.
 const game = new Game({
   trackLength: 30,
+  hero: hero1,
+  enemy: new Enemy(),
+  view: new View(),
 });
-
 
 // Запуск игры.
 game.play();
+game.view.render();
+game.hero.moveRight();
