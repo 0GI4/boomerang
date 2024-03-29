89a177c6fc02dbb1c4260c17801fb72e36c7a282
0GI4 <pop9991@outlook.com>
2024-03-29 13:08:24 +0300

    smthWorks

--- run.js
+++ run.js
@@ -2,23 +2,37 @@
 // Запускает игру.
 const keypress = require('keypress');
 const Game = require('./src/Game');
-const View = require('./src/View');
-const Boomerang = require('./src/game-models/Boomerang');
-const Hero = require('./src/game-models/Hero');
-const Enemy = require('./src/game-models/Enemy');
-const runInteractiveConsole = require('./src/keyboard');
-
-const hero1 = new Hero(0);
 
 // Инициализация игры с настройками.
 const game = new Game({
   trackLength: 30,
-  hero: hero1,
-  enemy: new Enemy(),
-  view: new View(),
 });
 
 // Запуск игры.
+function gg(ch, key) {
+  if (key && key.name) {
+    switch (key.name) {
+      case 'a':
+        game.hero.moveLeft();
+        break;
+      case 'd':
+        game.hero.moveRight();
+        break;
+      case 'space':
+        game.attack();
+        break;
+      case 'c':
+        process.exit();
+        break;
+      default:
+        break;
+    }
+  }
+}
 game.play();
-game.view.render();
-game.hero.moveRight();
+
+
+keypress(process.stdin);
+process.stdin.on('keypress', gg);
+process.stdin.setRawMode(true);
+process.stdin.resume();
